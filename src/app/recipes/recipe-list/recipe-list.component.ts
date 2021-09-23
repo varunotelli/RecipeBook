463de3574fb4ae018fbae350b90625a2f0757fda
varunotelli <varun.kebab@gmail.com>
2021-09-24 01:43:07 +0530

    Finished part 1 of project

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
-@Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
-})
-export class RecipeListComponent implements OnInit {
-
-  constructor() { }
+@Component({ selector: 'app-recipe-list', templateUrl:
+'./recipe-list.component.html', styleUrls: ['./recipe-list.component.css'] })
+export class RecipeListComponent implements OnInit { recipes: Recipe[] = [ new
+Recipe("A test recipe", "This is a test",
+"https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg")
+]; constructor() { }
 
   ngOnInit(): void {
   }
